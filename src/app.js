ef09cd4102aafa8722a8d3189e12d82e3c7b1e21
Robson Rodrigues Da Silva <robson.silva@inmetrics.com.br>
2019-04-17 08:52:47 -0300

    Corrigido ploblema de conexão com banco

--- src/app.js
+++ src/app.js
@@ -6,8 +6,12 @@ const mongoose      =   require('mongoose')
 
 const app           =   express()
 
+
 // conecta ao banco
 mongoose.connect('mongodb://robsonpi3:robsonpi3@ds064278.mlab.com:64278/pi_iii')
+
+// carrega os models
+const shower = require('./models/monitring')
 
 // Carregar as rotas
 const index = require('./routes/index')
