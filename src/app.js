f07d09fb085901ef7ae6bebcdda54437f10166e2
Robson Rodrigues Da Silva <robson.silva@inmetrics.com.br>
2019-04-17 08:36:02 -0300

    cors habilitado

--- src/app.js
+++ src/app.js
@@ -5,13 +5,9 @@ const bodyParser    =   require('body-parser')
 const mongoose      =   require('mongoose')
 
 const app           =   express()
-const router        =   express.Router()
 
 // conecta ao banco
 mongoose.connect('mongodb://robsonpi3:robsonpi3@ds064278.mlab.com:64278/pi_iii')
-
-// carrega os models
-const shower = require('./models/monitring')
 
 // Carregar as rotas
 const index = require('./routes/index')
@@ -20,7 +16,13 @@ const sensorRoute = require('./routes/Routes')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-
+// Habilita o CORS
+app.use(function (req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+})
 
 app.use('/', index)
 app.use('/sensors', sensorRoute)
